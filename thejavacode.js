113ac7b482bd30733a5dd8986082f5104f1cde68
Martinkwd <martin.romero.carrasco@gmail.com>
2024-05-03 13:51:16 +1200

    changes made

--- thejavacode.js
+++ thejavacode.js
@@ -1,6 +1,18 @@
 function dice() {
-    var result = Math.floor(Math.random() * 6) + 1;
+    var result= randomNumber()
     document.getElementById("result").innerText = result
 }
 
+function randomNumber(){
+    var result = Math.floor(Math.random() * 6) + 1;
+    
+    return result  
+}
 
+// test unit
+
+var result= randomNumber() 
+if (result>=1&&result<=6){
+    console.log("test passed")
+
+}
